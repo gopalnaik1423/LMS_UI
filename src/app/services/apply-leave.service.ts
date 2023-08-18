43020ca03a917d23b77ef2bb911bc6ef4e135c37
basavarajiteosin <basavaraj@iteos.in>
2023-08-18 12:22:01 +0530

    Recent Done

--- src/app/services/apply-leave.service.ts
+++ src/app/services/apply-leave.service.ts
@@ -1,14 +1,17 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import * as XLSX from 'xlsx';
 @Injectable({
   providedIn: 'root'
 })
 export class ApplyLeaveService {
 
-  private baseUrl:string="https://localhost:7058/api/User/"
-  constructor(private http:HttpClient) { }
+  baseUrl!:string;
+  constructor(private http:HttpClient) { 
+    this.baseUrl=environment.baseUrl;
+  }
 
   errorHandler(error: HttpErrorResponse): Observable<string> {
     return throwError(

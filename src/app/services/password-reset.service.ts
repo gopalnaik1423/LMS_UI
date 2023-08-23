e50145cfdfac4259b9a493e2ccb899fc28af4d6a
basavarajiteosin <basavaraj@iteos.in>
2023-08-23 21:21:55 +0530

    Reset-pass & many cahnges

--- src/app/services/password-reset.service.ts
+++ src/app/services/password-reset.service.ts
@@ -1,19 +1,30 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { PasswordReset } from '../models/password-reset.model';
 import { environment } from 'src/environments/environment';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordResetService {
   baseUrl!:string;
+  errorHandler(error: HttpErrorResponse): Observable<string> {
+    return throwError(
+      () =>
+        error.error.error_description ||
+        error.error ||
+        error.message ||
+        'Server Error'
+    );
+  }
   constructor(private http: HttpClient) {
     this.baseUrl=environment.baseUrl;
   }
 
 
   newpasswordReset(passwordResetObj:PasswordReset){
-    return this.http.post<any>(`${this.baseUrl}reset-password`,passwordResetObj);
+    return this.http.post<any>(`${this.baseUrl}reset-password`,passwordResetObj)
+    .pipe(catchError(this.errorHandler));
   }
 }
--- src/app/services/reset-password.service.ts
+++ src/app/services/reset-password.service.ts
@@ -8,7 +8,7 @@ import { ResetPassword } from '../models/reset-password.model';
 export class ResetPasswordService {
 private baseUrl:string="https://localhost:7058/api/User"
   constructor(private http:HttpClient) { }
-
+  
   sendResetPasswordLink(email:string){
     return this.http.post<any>(`${this.baseUrl}/send-reset-email/${email}`,{})
   }
